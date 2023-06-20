3d1dd93fe7e253130019729faad2f9707db29c63
OneSummerDay <artem.kraievyi@gmail.com>
2023-06-20 19:11:26 +0300

    created class Student model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    students_number = models.PositiveBigIntegerField()
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    email = models.EmailField(max_length=25)
+    fields_of_study = models.CharField(max_length=30)
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return f'Student:{self.first_name} {self.last_name}'
+
+
+
